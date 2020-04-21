1dda9fd9654f55db44be366efa96650139d17246
thomaz <thomazsousa@outlook.com.br>
2020-04-21 12:21:57 -0300

    first files upload

---
+++ bundle.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var empresa = {
+  nome: 'Rocketseat',
+  endereco: {
+    cidade: 'Rio do Sul',
+    estado: 'SC'
+  }
+};
+var nome = empresa.nome,
+    _empresa$endereco = empresa.endereco,
+    cidade = _empresa$endereco.cidade,
+    estado = _empresa$endereco.estado;
+console.log(nome); // Rocketseat
+
+console.log(cidade); // Rio do Sul
+
+console.log(estado); // SC
+
+var usuario = {
+  nome: 'Diego',
+  idade: 23
+};
+
+function mostraInfo(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  return "".concat(nome, " tem ").concat(idade, " anos.");
+}
+
+console.log(mostraInfo(usuario));
